5477a3daac6c20b0beebed7660f3f13714db9dea
afrojani <sultana.ani2019@gmail.com>
2022-10-26 06:52:13 -0500

    single course page added

--- src/Pages/Courses/Courses/Courses.js
+++ src/Pages/Courses/Courses/Courses.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
 
 const Courses = () => {
     const course = useLoaderData();
     return (
         <div>
             <h2>this is courses section{course.name}</h2>
+            <Card border="primary">
+                <Card.Body >
+                    <Card.Text >
+                        <h5>{course.name}</h5>
+                        <h4>{course.title}</h4>
+                        <p>{course.body}</p>
+                        <h6>Course Duration: {course.time}</h6>
+
+                    </Card.Text>
+                </Card.Body>
+                <Card.Img className="img-fluid img-thumbnail" variant="bottom" src={course.image_url} />
+            </Card>
         </div>
     );
 };
